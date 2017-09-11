53ed6493dee01419545974ace3c72472755428eb
Cole Hudson <cole.hudson@wayne.edu>
2017-09-11 12:34:52 -0400

    typos and environmental variable fixes

--- install_scripts/hadoop.sh
+++ install_scripts/hadoop.sh
@@ -26,14 +26,16 @@ tar --strip-components=1 -xvf hadoop-$hadoop_version.tar.gz
 
 rm /opt/hadoop/hadoop-$hadoop_version.tar.gz
 
+HADOOP_PREFIX=/opt/hadoop
+
 cat <<EOT >> /etc/environment
 export HADOOP_PREFIX=/opt/hadoop
-export HADOOP_HOME=\$HADOOP_PREFIX
-export HADOOP_COMMON_HOME=\$HADOOP_PREFIX
-export HADOOP_CONF_DIR=\$HADOOP_PREFIX/etc/hadoop
-export HADOOP_HDFS_HOME=\$HADOOP_PREFIX
-export HADOOP_MAPRED_HOME=\$HADOOP_PREFIX
-export HADOOP_YARN_HOME=\$HADOOP_PREFIX
+export HADOOP_HOME=$HADOOP_PREFIX
+export HADOOP_COMMON_HOME=$HADOOP_PREFIX
+export HADOOP_CONF_DIR=$HADOOP_PREFIX/etc/hadoop
+export HADOOP_HDFS_HOME=$HADOOP_PREFIX
+export HADOOP_MAPRED_HOME=$HADOOP_PREFIX
+export HADOOP_YARN_HOME=$HADOOP_PREFIX
 EOT
 
 source /etc/environment
@@ -43,7 +45,7 @@ cp $SHARED_DIR/config/files/hdfs-site.xml $HADOOP_PREFIX/etc/hadoop/hdfs-site.xm
 cp $SHARED_DIR/config/files/core-site.xml $HADOOP_PREFIX/etc/hadoop/core-site.xml
 cp $SHARED_DIR/config/files/yarn-site.xml $HADOOP_PREFIX/etc/hadoop/yarn-site.xml
 
-sudo chown -R combine:combine opt/hadoop
+sudo chown -R combine:combine /opt/hadoop
 
 ## Start HDFS daemons
 # Format the namenode directory (DO THIS ONLY ONCE, THE FIRST TIME)
