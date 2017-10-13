da59d4b5962860a59c5ec3c98a4061abb7a22ed8
Graham Hukill <ghukill@gmail.com>
2017-10-13 12:04:45 -0400

    Set DB to utf8 encoding

--- config/files/combine.sql
+++ config/files/combine.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE combine;
+CREATE DATABASE combine CHARACTER SET utf8 COLLATE utf8_general_ci;;
 CREATE USER 'combine'@'localhost' IDENTIFIED BY 'combine';
 GRANT ALL PRIVILEGES ON * . * TO 'combine'@'localhost';
 FLUSH PRIVILEGES;
